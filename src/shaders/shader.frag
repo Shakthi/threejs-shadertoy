a3223d6b081e4c7e179d06b366143ef8f464e9ad
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-07-22 23:44:13 +0530

    Fixed basic shadertoy shader

--- src/shaders/shader.frag
+++ src/shaders/shader.frag
@@ -1,5 +1,12 @@
 precision highp float;
-
+uniform vec2 iResolution;
+uniform float iTime;
+varying vec2 vUv;
+void  mainImage( out vec4,  vec2 fragCoord );
 void main () {
-    gl_FragColor = vec4(1.0);
+    //out const vec4 outcolor;
+    //mainImage(iResolution*vUv);
+    vec4 outfrag;
+    mainImage(outfrag,iResolution*vUv);
+    gl_FragColor = outfrag;
 }
--- src/three.js
+++ src/three.js
@@ -3,6 +3,7 @@ import OrbitControls from 'orbit-controls-es6'
 
 import vert from './shaders/shader.vert'
 import frag from './shaders/shader.frag'
+import shaderToySample from './shaders/shadertoySample.frag'
 
 // Initial HMR Setup
 if (module.hot) {
@@ -22,6 +23,7 @@ if (module.hot) {
 // Three Scene
 let scene, camera, renderer, animationId, controls
 let geometry, material, mesh
+let clock;
 
 function init() {
     scene = new THREE.Scene()
@@ -35,16 +37,23 @@ function init() {
     camera.position.z = 1000
 
     controls = new OrbitControls(camera)
+    var finalfrag =  frag+"\n"+shaderToySample;
+    geometry = new THREE.PlaneBufferGeometry(1500, 750);
+    clock = new THREE.Clock();
 
-    geometry = new THREE.PlaneBufferGeometry(1500, 750)
     material = new THREE.RawShaderMaterial({
         vertexShader: vert,
-        fragmentShader: frag
-    })
-    
-    var texture = new THREE.TextureLoader().load( 'resources/UV_Grid_Sm.jpg' );
+        fragmentShader: finalfrag ,
+        uniforms: {
+            iTime: { type: "1f", value: clock.getElapsedTime() },
+            iResolution:{value:new THREE.Vector2(1500, 750)}
 
-// immediately use the te
+        }
+    })
+
+    var texture = new THREE.TextureLoader().load('resources/UV_Grid_Sm.jpg');
+
+    // immediately use the te
 
     // material = new THREE.MeshBasicMaterial({
     //     map:texture
@@ -53,7 +62,7 @@ function init() {
     mesh = new THREE.Mesh(geometry, material)
     scene.add(mesh)
 
-    
+
     renderer = new THREE.WebGLRenderer({ antialias: true })
     renderer.setSize(window.innerWidth, window.innerHeight)
 
@@ -63,10 +72,11 @@ function init() {
 function animate() {
     animationId = requestAnimationFrame(animate)
 
-  
+
     mesh.rotation.y += 0.0001
 
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
+    material.uniforms.iTime.value = clock.getElapsedTime();
 }
 
 init()
